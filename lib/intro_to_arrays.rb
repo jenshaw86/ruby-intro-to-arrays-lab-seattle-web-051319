b0d7903ab4112508abe6b6d88211a6a103455e7e
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-05 05:54:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,9 +7,13 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array[0]
+  array.first
 end
 
 def third_element(array)
   array[2]
+end
+
+def last_element(array)
+  array[-1]
 end
